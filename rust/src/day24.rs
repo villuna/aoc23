0dc8b30efe248b97a01b24a6377aa075f522b4d1
Luna Borella <ulfrthewolf@gmail.com>
2023-12-25 14:52:47 +1000

    cargo fmt

--- rust/src/day24.rs
+++ rust/src/day24.rs
@@ -83,7 +83,9 @@ async fn day24_async(input: &str, ctx: &mut AOContext) {
     let input_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
         label: Some("input buffer"),
         contents: bytemuck::cast_slice(&stones),
-        usage: wgpu::BufferUsages::STORAGE | wgpu::BufferUsages::COPY_DST | wgpu::BufferUsages::COPY_SRC,
+        usage: wgpu::BufferUsages::STORAGE
+            | wgpu::BufferUsages::COPY_DST
+            | wgpu::BufferUsages::COPY_SRC,
     });
 
     let result_buffer = device.create_buffer(&wgpu::BufferDescriptor {
@@ -100,47 +102,51 @@ async fn day24_async(input: &str, ctx: &mut AOContext) {
         mapped_at_creation: false,
     });
 
-    let input_bind_group_layout = device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor { 
-        label: Some("input bind group layout"),
-        entries: &[
-            wgpu::BindGroupLayoutEntry {
-                binding: 0,
-                visibility: wgpu::ShaderStages::COMPUTE,
-                ty: wgpu::BindingType::Buffer { 
-                    ty: wgpu::BufferBindingType::Uniform, 
-                    has_dynamic_offset: false, 
-                    min_binding_size: None,
+    let input_bind_group_layout =
+        device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
+            label: Some("input bind group layout"),
+            entries: &[
+                wgpu::BindGroupLayoutEntry {
+                    binding: 0,
+                    visibility: wgpu::ShaderStages::COMPUTE,
+                    ty: wgpu::BindingType::Buffer {
+                        ty: wgpu::BufferBindingType::Uniform,
+                        has_dynamic_offset: false,
+                        min_binding_size: None,
+                    },
+                    count: None,
                 },
-                count: None,
-            },
-            wgpu::BindGroupLayoutEntry {
-                binding: 1,
-                visibility: wgpu::ShaderStages::COMPUTE,
-                ty: wgpu::BindingType::Buffer {
-                    ty: wgpu::BufferBindingType::Storage { read_only: true },
-                    has_dynamic_offset: false,
-                    min_binding_size: NonZeroU64::new((stones.len() * size_of::<Vertex>()) as u64),
+                wgpu::BindGroupLayoutEntry {
+                    binding: 1,
+                    visibility: wgpu::ShaderStages::COMPUTE,
+                    ty: wgpu::BindingType::Buffer {
+                        ty: wgpu::BufferBindingType::Storage { read_only: true },
+                        has_dynamic_offset: false,
+                        min_binding_size: NonZeroU64::new(
+                            (stones.len() * size_of::<Vertex>()) as u64,
+                        ),
+                    },
+                    count: None,
                 },
-                count: None,
-            }
-        ]
-    });
+            ],
+        });
 
-    let result_bind_group_layout = device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor { 
-        label: Some("result bind group layout"),
-        entries: &[
-            wgpu::BindGroupLayoutEntry {
+    let result_bind_group_layout =
+        device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
+            label: Some("result bind group layout"),
+            entries: &[wgpu::BindGroupLayoutEntry {
                 binding: 0,
                 visibility: wgpu::ShaderStages::COMPUTE,
                 ty: wgpu::BindingType::Buffer {
                     ty: wgpu::BufferBindingType::Storage { read_only: false },
                     has_dynamic_offset: false,
-                    min_binding_size: NonZeroU64::new((stones.len() * stones.len() * size_of::<u32>()) as u64),
+                    min_binding_size: NonZeroU64::new(
+                        (stones.len() * stones.len() * size_of::<u32>()) as u64,
+                    ),
                 },
                 count: None,
-            }
-        ]
-    });
+            }],
+        });
 
     let pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
         label: Some("compute pipeline layout"),
@@ -157,18 +163,17 @@ async fn day24_async(input: &str, ctx: &mut AOContext) {
 
     let data_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
         label: Some("length buffer"),
-        contents: bytemuck::cast_slice(&[
-            DataStruct {
-                min: 200000000000000.0,
-                max: 400000000000000.0,
-                length: [stones.len() as u32, 0, 0, 0],
-            }]),
+        contents: bytemuck::cast_slice(&[DataStruct {
+            min: 200000000000000.0,
+            max: 400000000000000.0,
+            length: [stones.len() as u32, 0, 0, 0],
+        }]),
         usage: wgpu::BufferUsages::UNIFORM,
     });
 
-    let input_bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor { 
-        label: Some("input bind group"), 
-        layout: &input_bind_group_layout, 
+    let input_bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
+        label: Some("input bind group"),
+        layout: &input_bind_group_layout,
         entries: &[
             wgpu::BindGroupEntry {
                 binding: 0,
@@ -177,28 +182,26 @@ async fn day24_async(input: &str, ctx: &mut AOContext) {
             wgpu::BindGroupEntry {
                 binding: 1,
                 resource: input_buffer.as_entire_binding(),
-            }
-        ], 
+            },
+        ],
     });
 
-    let result_bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor { 
-        label: Some("result bind group"), 
-        layout: &result_bind_group_layout, 
-        entries: &[
-            wgpu::BindGroupEntry {
-                binding: 0,
-                resource: result_buffer.as_entire_binding(),
-            }
-        ], 
+    let result_bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
+        label: Some("result bind group"),
+        layout: &result_bind_group_layout,
+        entries: &[wgpu::BindGroupEntry {
+            binding: 0,
+            resource: result_buffer.as_entire_binding(),
+        }],
     });
 
-    let mut encoder = device.create_command_encoder(&wgpu::CommandEncoderDescriptor { 
-        label: Some("command encoder") 
+    let mut encoder = device.create_command_encoder(&wgpu::CommandEncoderDescriptor {
+        label: Some("command encoder"),
     });
 
-    let mut pass = encoder.begin_compute_pass(&wgpu::ComputePassDescriptor { 
-        label: Some("compute pass"), 
-        timestamp_writes: None, 
+    let mut pass = encoder.begin_compute_pass(&wgpu::ComputePassDescriptor {
+        label: Some("compute pass"),
+        timestamp_writes: None,
     });
 
     pass.set_pipeline(&compute_pipeline);
@@ -208,7 +211,13 @@ async fn day24_async(input: &str, ctx: &mut AOContext) {
 
     drop(pass);
 
-    encoder.copy_buffer_to_buffer(&result_buffer, 0, &output_buffer, 0, (size_of::<u32>() * stones.len() * stones.len()) as u64);
+    encoder.copy_buffer_to_buffer(
+        &result_buffer,
+        0,
+        &output_buffer,
+        0,
+        (size_of::<u32>() * stones.len() * stones.len()) as u64,
+    );
     queue.submit(Some(encoder.finish()));
 
     let buf_slice = output_buffer.slice(..);
@@ -222,8 +231,8 @@ async fn day24_async(input: &str, ctx: &mut AOContext) {
         ctx.submit_part1(data.iter().sum::<u32>());
     }
 
-
-    ctx.submit_part2(0); }
+    ctx.submit_part2(0);
+}
 
 pub fn day24(input: String, ctx: &mut AOContext) {
     pollster::block_on(day24_async(&input, ctx));
